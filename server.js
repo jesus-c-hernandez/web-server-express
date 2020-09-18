fc7d83957120b7bc9121d9e1af45303e88196428
Jesus Hdez <Jessiehdez36@hotmail.com>
2020-09-17 21:44:09 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express();
+const hbs = require('hbs');
+
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/partials', function(err) {});
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', (req, res) => {
+    // res.send('Hello World')
+
+    res.render('home', {
+        nombre: 'jesus',
+    });
+
+});
+
+app.get('/about', (req, res) => {
+    // res.send('Hello World')
+
+    res.render('about', {});
+
+});
+
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port } `);
+});
